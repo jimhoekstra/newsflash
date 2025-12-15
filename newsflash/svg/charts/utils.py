595bf856aaca8f639fd1be22aa4ffdd73c521d04
Jim Hoekstra <jimhoekstra2@gmail.com>
2025-12-15 21:19:20 +0100

    undo breaking change

--- newsflash/svg/charts/utils.py
+++ newsflash/svg/charts/utils.py
@@ -14,20 +14,30 @@ def get_y_label_positions(
     min_y: float | int | None = None,
     max_y: float | int | None = None,
     divide_by: int = 4,
+    recursive_call: bool = True,
 ) -> list[float] | list[int]:
     min_y_to_use = min(values) if min_y is None else min_y
     max_y_to_use = max(values) if max_y is None else max_y
 
-    min_y_to_use = nice_round(min_y_to_use, reference=max_y_to_use)
-    max_y_to_use = nice_round(max_y_to_use, reference=max_y_to_use)
+    step = nice_ceil((max_y_to_use - min_y_to_use) / divide_by)
 
-    step = (max_y_to_use - min_y_to_use) / divide_by
+    min_y_rounded = floor(min_y_to_use / step) * step
+    max_y_rounded = ceil(max_y_to_use / step) * step
 
     y_label_positions = []
-    current_label = min_y_to_use
-    while current_label <= max_y_to_use:
+    current_label = min_y_rounded
+    while current_label <= max_y_rounded:
         y_label_positions.append(current_label)
         current_label += step
+
+    if recursive_call and len(y_label_positions) > (divide_by + 1):
+        return get_y_label_positions(
+            values=values,
+            min_y=min_y,
+            max_y=max_y,
+            divide_by=divide_by - 1,
+            recursive_call=False,
+        )
 
     return y_label_positions
 
@@ -36,9 +46,3 @@ def nice_ceil(x: float) -> float:
     oom = order_of_magnitude(x)
     factor = pow(10, oom)
     return ceil(x / factor) * factor
-
-
-def nice_round(x: float, reference: float) -> float:
-    oom = order_of_magnitude(reference)
-    factor = pow(10, oom)
-    return round(x / factor) * factor
